4e4a7beb7bdbfa9ca445542ab1fb74209982372d
Jorge Luis Arencibia <jorge9815rdz@gmail.com>
2025-03-13 21:55:55 -0400

    feat: code migration
    + project set up
    + extracted code from mapper module on uprChat

---
+++ hd2neo4j/routes/mapper_routes.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, File
+from typing import Annotated
+from uprchat.mapper.services import MapperService, RepositoryService
+
+rt = APIRouter(
+    prefix="/mapper",
+    tags=["mapper"],
+)
+
+
+@rt.get("/graph")
+def get_graph():
+    response = RepositoryService().get_graph()
+    return response.records
+
+
+@rt.get("/query")
+def execute_query(query: str):
+    return RepositoryService().execute_external_query(query)
+
+
+@rt.post("/start")
+def start_mapping(
+    configFile: Annotated[bytes, File()],
+    dataFile: Annotated[bytes, File()],
+):
+    controller = MapperService(configFile, dataFile)
+    controller.start_mapping()
+
+
+@rt.post("/drop")
+def drop_db():
+    RepositoryService().clean_graph_db()
